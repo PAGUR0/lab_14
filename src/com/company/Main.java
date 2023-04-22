12b9e17c36efc92ef879e8ff107ea0a1d76d4c90
Paguro <ivankosaev2006@gmail.com>
2023-04-22 08:19:33 +0300

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,15 +9,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader("program.txt"));
         BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt", true));
-        Pattern pattern = Pattern.compile("(/{2}.*$)|(/\\\\*.*?\\\\*/)", Pattern.DOTALL);
-        String str;
-        String input = "";
-        while ((str = reader.readLine()) != null) {
-            input = input + "\n" + str;
+        Pattern pattern = Pattern.compile("(/{2}[^\n]*\n)|(/\\\\*.*?\\\\*/)", Pattern.DOTALL);
+        int c;
+        StringBuilder input = new StringBuilder();
+        while ((c = reader.read()) != -1) {
+            input.append((char) c);
         }
-        Matcher matcher = pattern.matcher(input);
-        str = matcher.replaceAll("");
-        writer.write(str);
+        Matcher matcher = pattern.matcher(input.toString());
+        input = new StringBuilder(matcher.replaceAll(""));
+        writer.write(input.toString());
         reader.close();
         writer.close();
     }
